ca3d77183397a1bd2e4ee45fbbbcd1cdaf2382f0
Rishav <paulrishav008@gmail.com>
2024-09-05 23:27:10 +0530

    added admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: "https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 }, {timestamps: true});
 
 const User = mongoose.model('User' , userSchema);
