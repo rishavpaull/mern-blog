0c4c5b28d91b8cbd876d25fa1f20a92d63c61119
Rishav <paulrishav008@gmail.com>
2024-09-06 01:16:20 +0530

    added upload blog functionality(both backend and frontend)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -12,9 +12,10 @@ import { updateFailure , updateStart , updateSuccess , deleteUserFailure , delet
 import { Modal } from 'flowbite-react';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import {Link} from 'react-router-dom'
 
 export default function DashProfile() {
-    const {currentUser , error} = useSelector(state => state.user);
+    const {currentUser , error , loading} = useSelector(state => state.user);
     const [imageFile , setImageFile] = useState(null);
     const [imageFileUrl , setImageFileUrl] = useState(null);
     const [imageFileUpLoadProgress , setImageFileUploadProgress] = useState(null);
@@ -195,9 +196,25 @@ export default function DashProfile() {
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
 
             <TextInput type='password' id='password' placeholder='password'  onChange={handleChange}/>
-            <Button type='submit' gradientDuoTone="purpleToBlue" outline>
-                Update
+            <Button
+              type='submit'
+              gradientDuoTone='purpleToBlue'
+              outline
+              disabled={loading || imageFileUploading}
+            >
+              {loading ? 'Loading...' : 'Update'}
             </Button>
+            {currentUser.isAdmin && (
+              <Link to={'/create-post'}>
+                <Button
+                  type='button'
+                  gradientDuoTone='purpleToPink'
+                  className='w-full'
+                >
+                  Create a post
+                </Button>
+              </Link>
+          )}
         </form>
         <div className='text-red-500 flex justify-between'>
             <span onClick={() => setShowModal(true)}className='cursor-pointer'>Delete Account</span>
